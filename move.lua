11d83cab615c45a08d4c05791c27d922059e8858
Ryan Scott <ryan.scott@altra.io>
2021-05-20 19:58:46 -0400

    Basic movements added to Move API

--- move.lua
+++ move.lua
@@ -1,7 +1,42 @@
-local turtle = require("turtle")
+if not turtle then
+	return error("Must be a turtle to use the Move API.")
+end
 
-function turtle.forward(num)
-	for i=num, 0, -1 do
+function turtle.go(num)
+	for i=0,num,1 do
 		turtle.forward()
 	end
+end
+
+function turtle.goBack(num)
+	for i = 1, num, 1 do
+		turtle.back()
+	end
+end
+
+function turtle.goRight(num)
+	turtle.turnRight()
+	turtle.go(num)
+end
+
+function turtle.goLeft(num)
+	turtle.turnRight()
+	turtle.go(num)
+end
+
+function turtle.goUp(num)
+	for i = 1, num, 1 do
+		turtle.up()
+	end
+end
+
+function turtle.goDown(num)
+	for i = 1, num, 1 do
+		turtle.down()
+	end
+end
+
+function turtle.turnAround()
+	turtle.turnRight()
+	turtle.turnRight()
 end
